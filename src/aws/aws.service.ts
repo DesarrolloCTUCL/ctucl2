f939ead333681251e2e08655b6643f877ac18558
CristianDv7 <christian-502@hotmail.com>
2025-04-09 20:21:29 -0500

    Se agrego el sdk v3 de AWS

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,42 +1,46 @@
 // src/aws/aws.service.ts
 
 import { Injectable } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
+import { RDSClient, DescribeDBInstancesCommand } from '@aws-sdk/client-rds';
+
 import * as dotenv from 'dotenv';
 dotenv.config();  // Carga las variables de entorno desde un archivo .env
 @Injectable()
 export class AwsService {
-  private rds: AWS.RDS;
+  private rdsClient: RDSClient;
 
   constructor() {
-    AWS.config.update({ region: process.env.AWS_REGION });  // Usa la región desde las variables de entorno
-    this.rds = new AWS.RDS();  // Inicializa el cliente RDS de AWS SDK
+    this.rdsClient = new RDSClient({ region: process.env.AWS_REGION });
   }
 
   // Método para obtener las credenciales de conexión de la base de datos RDS
   async getRDSCredentials(dbInstanceIdentifier: string) {
     try {
-      const result = await this.rds.describeDBInstances({ DBInstanceIdentifier: dbInstanceIdentifier }).promise();
-        // Validar que DBInstances esté definido y tenga al menos una instancia
+      const command = new DescribeDBInstancesCommand({
+        DBInstanceIdentifier: dbInstanceIdentifier,
+      });
+
+      const result = await this.rdsClient.send(command);
+
       if (!result.DBInstances || result.DBInstances.length === 0) {
         throw new Error('No se encontró ninguna instancia de base de datos.');
       }
-      const dbInstance = result.DBInstances[0];  // Accede a la primera instancia
 
-      // Verifica si 'Endpoint' existe
+      const dbInstance = result.DBInstances[0];
+
       if (!dbInstance.Endpoint) {
         throw new Error('No se encontró el endpoint de la base de datos.');
       }
-      // Obtener la dirección y el puerto del endpoint de la base de datos
+
       const endpoint = dbInstance.Endpoint.Address;
       const port = dbInstance.Endpoint.Port;
 
       return {
         host: endpoint,
         port: port,
-        username: process.env.DB_USERNAME,  // Nombre de usuario desde las variables de entorno
-        password: process.env.DB_PASSWORD,  // Contraseña desde las variables de entorno
-        database: process.env.DB_NAME,     // Nombre de la base de datos desde las variables de entorno
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
       };
     } catch (error) {
       console.error('Error al obtener las credenciales de RDS:', error);
